9da5a7111ce0da32af3d647b0a11f46a7dcc5d10
JAY LAKHANI <88767949+20IT062@users.noreply.github.com>
2022-09-09 12:48:33 +0530

    Added Students Listing.

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Container, Table } from "react-bootstrap";
+import axios from "axios";
 
 export default function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
+  };
   return (
     <div className="my-3">
       <Container>
-        <Card.Header><h3>Students List</h3></Card.Header>
+        <Card.Header>
+          <h3>Students List</h3>
+        </Card.Header>
         <Card.Body>
           <Table striped bordered hover>
-          <thead>
-        <tr>
-          <th>Student Id</th>
-          <th>Student Name</th>
-          <th>Student Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Jay</td>
-          <td>Lakhani</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Parth</td>
-          <td>Mangukiya</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Jay</td>
-          <td>Italiya</td>
-        </tr>
-      </tbody>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                </tr>
+                )
+              )}
+            </tbody>
           </Table>
         </Card.Body>
       </Container>
